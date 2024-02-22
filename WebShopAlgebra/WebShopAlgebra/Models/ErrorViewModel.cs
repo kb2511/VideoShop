e002e0123ed41efddc7289d263b0b70ae602beff
Katarina Blažević <katarina.blazevic@iusinfo.hr>
2024-02-22 10:32:30 +0100

    MVC project created

---
+++ WebShopAlgebra/WebShopAlgebra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShopAlgebra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
